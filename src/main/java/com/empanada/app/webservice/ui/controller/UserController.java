4f92236fd1793ed1e53387473f4014f970bb90b0
Capitan Empanada <ivanlp10n2@gmail.com>
2019-04-25 13:44:32 -0300

    Added sql-connection and dependencies

--- src/main/java/com/empanada/app/webservice/ui/controller/UserController.java
+++ src/main/java/com/empanada/app/webservice/ui/controller/UserController.java
@@ -1,5 +1,34 @@
 package com.empanada.app.webservice.ui.controller;
 
-public class UserController {
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("users") // http://localhost:8080/users
+public class UserController {
+	
+	@GetMapping
+	public String getUserInformation () {
+		return "something";
+	}
+	
+	
+	@PostMapping
+	public String createUser () {
+		return "create userhas been used";
+	}
+	
+	@PutMapping
+	public String updateUser () {
+		return "update";
+	}
+	
+	@DeleteMapping
+	public String deleteUser () {
+		return "delete";
+	}
 }
--- src/test/java/com/empanada/app/webservice/SpringWsAppApplicationTests.java
+++ src/test/java/com/empanada/app/webservice/SpringWsAppApplicationTests.java
@@ -9,8 +9,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringWsAppApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
 
 }
