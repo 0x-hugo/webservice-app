c323fdd90e5440c92cf368b5c1177039c3f51d76
Capitan Empanada <ivanlp10n2@gmail.com>
2019-04-26 05:25:15 -0300

    check if email already exist by Spring findBy utils

--- src/main/java/com/empanada/app/webservice/impl/UserServiceImpl.java
+++ src/main/java/com/empanada/app/webservice/impl/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package com.empanada.app.webservice.impl;
 
+import javax.management.RuntimeErrorException;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.empanada.app.webservice.UserReporsitory;
+import com.empanada.app.webservice.UserRepository;
 import com.empanada.app.webservice.io.entity.UserEntity;
 import com.empanada.app.webservice.service.UserService;
 import com.empanada.app.webservice.shared.dto.UserDto;
@@ -14,16 +16,21 @@ import com.empanada.app.webservice.shared.dto.UserDto;
 public class UserServiceImpl implements UserService{
 
 	@Autowired
-	UserReporsitory userRepository;
+	UserRepository userRepository;
 	
 	@Override
 	public UserDto createUser(UserDto user) {
+		
+		//check if email address already exist
+		if (userRepository.findByEmail(user.getEmail()) != null) throw new RuntimeException("The user already exist");
+		
 		UserEntity userEntity = new UserEntity();
 		BeanUtils.copyProperties(user, userEntity);
 		
 		//HardCode for test
 		userEntity.setUserId("testUser");
 		userEntity.setEncryptedPassword("tesst");
+		
 		
 		UserEntity storedUserDetails = userRepository.save(userEntity);
 		
--- src/main/java/com/empanada/app/webservice/io/entity/UserEntity.java
+++ src/main/java/com/empanada/app/webservice/io/entity/UserEntity.java
@@ -27,7 +27,7 @@ public class UserEntity implements Serializable{
 	@Column(nullable = false, length = 50)
 	private String lastName;
 	
-	@Column(nullable = false, length = 50)
+	@Column(nullable = false, length = 50, unique = true)
 	private String email;
 	
 	@Column(nullable = false)
