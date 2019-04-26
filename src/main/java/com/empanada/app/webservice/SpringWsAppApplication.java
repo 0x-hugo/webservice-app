aa7520eeffbd49b3b9c52741b761ec59ed9663f9
Capitan Empanada <ivanlp10n2@gmail.com>
2019-04-26 06:19:04 -0300

    fixed Bean Injection Error and allowed HTTP post for /users

--- src/main/java/com/empanada/app/webservice/SpringWsAppApplication.java
+++ src/main/java/com/empanada/app/webservice/SpringWsAppApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @SpringBootApplication
 public class SpringWsAppApplication {
@@ -14,7 +14,7 @@ public class SpringWsAppApplication {
 	}
 	
 	@Bean
-	public PasswordEncoder passwordEncoder() {
+	public BCryptPasswordEncoder passwordEncoder() {
 	    return new BCryptPasswordEncoder();
 	}
 
