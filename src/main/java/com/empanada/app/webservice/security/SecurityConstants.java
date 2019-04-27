63950bbf3dd2191ca67e8acfede176bcc812f7d2
Capitan Empanada <ivanlp10n2@gmail.com>
2019-04-27 08:53:40 -0300

    Added comments for Constant (sometimes it's hard to read)

--- src/main/java/com/empanada/app/webservice/security/SecurityConstants.java
+++ src/main/java/com/empanada/app/webservice/security/SecurityConstants.java
@@ -2,10 +2,10 @@ package com.empanada.app.webservice.security;
 
 public class SecurityConstants {
 	
-	public static final long EXPIRATION_DATE = 864000000; //10 days in milisec
-	public static final String TOKEN_PREFIX = "Bearer ";
-	public static final String HEADER_STRING = "Authorization";
-	public static final String SIGN_UP_URL = "/users";
-	public static final String TOKEN_SECRET = "jf9i4jgu83nfl0";
+	public static final long EXPIRATION_DATE = 864000000; 		//10 days in milisec
+	public static final String TOKEN_PREFIX = "Empanada ";		//Prefix for token creator
+	public static final String HEADER_STRING = "Authorization"; //Name of Header for authentication
+	public static final String SIGN_UP_URL = "/users"; 			//URL for request 
+	public static final String TOKEN_SECRET = "jf9i4jgu83nfl0";	//Secret generator
 
 }
