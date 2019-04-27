70514b44fcb755ff19ea2eb39a5b6347dea9814a
Capitan Empanada <ivanlp10n2@gmail.com>
2019-04-27 09:47:34 -0300

    Secret token readed from application.properties

---
+++ src/main/java/com/empanada/app/webservice/security/AppProperties.java
@@ -0,0 +1,16 @@
+package com.empanada.app.webservice.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppProperties {
+
+	@Autowired
+	private Environment env;
+	
+	public String getSecretToken() {
+		return env.getProperty("TOKEN_SECRET");
+	}
+}
--- src/main/java/com/empanada/app/webservice/security/SecurityConstants.java
+++ src/main/java/com/empanada/app/webservice/security/SecurityConstants.java
@@ -1,11 +1,17 @@
 package com.empanada.app.webservice.security;
 
+import com.empanada.app.webservice.SpringApplicationContext;
+
 public class SecurityConstants {
 	
 	public static final long EXPIRATION_DATE = 864000000; 		//10 days in milisec
 	public static final String TOKEN_PREFIX = "Empanada ";		//Prefix for token creator
 	public static final String HEADER_STRING = "Authorization"; //Name of Header for authentication
 	public static final String SIGN_UP_URL = "/users"; 			//URL for request 
-	public static final String TOKEN_SECRET = "jf9i4jgu83nfl0";	//Secret generator
+//	public static final String TOKEN_SECRET = "jf9i4jgu83nfl0";	//Secret generator
 
+	public static String getTokenSecret() {
+		AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("appProperties");
+		return appProperties.getSecretToken();
+	}
 }
