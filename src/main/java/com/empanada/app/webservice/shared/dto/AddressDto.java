f6dc7247d99855179ab2c6d37a366d1e68ee21b3
Capitan Empanada <ivanlp10n2@gmail.com>
2019-05-05 18:50:41 -0300

    Added dependency ModelMapper for better relationship mapping of DTO

---
+++ src/main/java/com/empanada/app/webservice/shared/dto/AddressDto.java
@@ -0,0 +1,48 @@
+package com.empanada.app.webservice.shared.dto;
+
+public class AddressDto {
+	
+	private long id;
+	private String city;
+	private String country;
+	private String streetName;
+	private String postalCode;
+	private UserDto userDetails;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getStreetName() {
+		return streetName;
+	}
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	public UserDto getUserDetails() {
+		return userDetails;
+	}
+	public void setUserDetails(UserDto userDetails) {
+		this.userDetails = userDetails;
+	}
+}
