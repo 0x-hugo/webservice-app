ab125794014ecaab3b803ea1ef4f8ea7c9a95de6
Capitan Empanada <ivanlp10n2@gmail.com>
2020-03-11 22:32:32 -0300

    formatting

--- src/main/java/com/empanada/app/webservice/SpringApplicationContext.java
+++ src/main/java/com/empanada/app/webservice/SpringApplicationContext.java
@@ -1,10 +1,8 @@
 package com.empanada.app.webservice;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.annotation.Bean;
 
 public class SpringApplicationContext implements ApplicationContextAware {
   private static ApplicationContext context;
